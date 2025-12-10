ab19019e22ff55454b07825480086842e196e8b3
kavyabammidi <121555354+kavyabammidi@users.noreply.github.com>
2025-12-10 10:04:53 +0530

    routes added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,9 @@
 import { Routes } from '@angular/router';
-import { JobListComponent } from './jobs/job-list/job-list.component';
+import { JobListComponent } from './Components/job-list/job-list.component';
 
 export const routes: Routes = [
-<<<<<<< HEAD
-    // {path : '', redirectTo: 'jobs', pathMatch: 'full' },
     { path: 'jobs', component: JobListComponent },
-=======
    
     {path: 'login', loadComponent: () => import('./Components/Auth/loginpage/loginpage.component').then(m => m.LoginpageComponent)},
     {path: 'register', loadComponent: () => import('./Components/Auth/registration-page/registration-page.component').then(m => m.RegistrationPageComponent)},
->>>>>>> b45dfcfd46cfc8132ff4d8c11adfd8037a2e7104
 ];
